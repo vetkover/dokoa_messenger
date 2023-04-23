bb984c34c97e0eec4705c8b22808ff4de5333733
vetkover <vetkover@gmail.com>
2023-04-23 03:20:02 +0200

    savepoint

--- frontend/src/components/Pages/Main/Main.js
+++ frontend/src/components/Pages/Main/Main.js
@@ -1,5 +1,5 @@
-import React, { Component, createRef, useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes, Router, useNavigate, UNSAFE_DataRouterStateContext } from "react-router-dom";
+import React, { Component, createRef, useState, useEffect, useRef, useLayoutEffect } from 'react';
+import { BrowserRouter, Route, Routes, Router, useNavigate } from "react-router-dom";
 import "../Main/Main.scss"
 import { whoami } from "../../Modules/api/whoami"
 
@@ -17,7 +17,14 @@ function MessagelistContent(){
     window.setupChatId = setupChatId;
     window.isChatId = isChatId;
 
+
+      const [whoamiData, setWhoamiData] = useState(null);
     useEffect(() => {
+      const whoamiData = window.whoamiResult.then((data) => setWhoamiData(data));
+      if (!isLoading && isChatId != null) {
+
+      }
+
 
       }, []);
 
@@ -31,65 +38,40 @@ function MessagelistContent(){
     }  
     if (isChatId == null) {
       return <div>choose a chat</div>;
-    } else if (!isLoading && isChatId != null) {
-        const userList = data.result.map((item, index) => (
-            <div className='chatWindow-content' id='myMessage' key={index}>
-            <div className='senderAvatar'>
-                <img src={avatar}></img>
-            </div>
-            <div className='chatMain-block'>
-                <div className='messageData'>
-            <p>{item.author}</p>
-            <p>{item.time}</p>
+} else if (!isLoading && isChatId != null) {
+      const userList = data.result.map((item, index) => {
+      const isMyMessage = item.author == whoamiData.id;
+      const messageId = isMyMessage ? 'myMessage' : 'otherMessage';
+      return (
+        <div className='chatWindow-content' id={messageId} key={index}>
+          <div className='senderAvatar'>
+            <img src={avatar}></img>
+          </div>
+          <div className='chatMain-block'>
+            <div className='messageData'>
+              <p>{item.author}</p>
+              <p>{item.time}</p>
             </div>
             <p>{item.text}</p>
-            </div>
+          </div>
         </div>
-          ));
-      return <div>{userList}</div>;;
+      )});
+    return <React.Fragment> {userList} </React.Fragment>
   }
 }
 
-function ChatlistContent(){
-    const [isLoading, setIsLoading] = useState(true);
-    const [data, setData] = useState([]);
-    const navigate = useNavigate();
 
-    whoami(navigate)
-    useEffect(() => {
-        fetch("/api/message/chatList?id=1")
-          .then((response) => response.json())
-          .then((data) => {
-            setData(data);
-            setIsLoading(false);
-          });
-      }, []);
-
-      
-    if (isLoading) {
-      return <div>Loading...</div>;
-    } else {
-const userList = data.result.map((item) => {
-  const lastMessage = item.messages || {};
-  return (
-    <div onClick={() => {window.setupChatId(item.chatid)}} className='chatList-content' key={item.chatid}>
-      <img id='userAvatar' src={avatar}></img>
-      <p id='userName'>{lastMessage.author || ""}</p>
-      <p id='lastMessageTime'>{lastMessage.time || ""}</p>
-      <p id='lastMessageText'>{lastMessage.text || ""}</p>
-    </div>
-  );
-});
-      return <div>{userList}</div>;;
-  }
-}
 
 class Main extends Component{
 constructor(props) {
     super(props);
     this.inputMessageBox = createRef();
+    this.chatlistContent = createRef();
     this.sendMessage = this.sendMessage.bind(this);
     
+  }
+
+  componentDidMount(){
   }
   sendMessage(){
     fetch(`/api/message/sendMessage?id=${window.isChatId}`, {
@@ -104,12 +86,14 @@ constructor(props) {
     .then(response => response.json())
   }
 
+
+  
 render (){
     return(
         <div className='main-container'>
-            <div className='chatList-container'>
+            <div className='chatList-container' ref={this.chatlistContent}>
                 <div className='chatList-listContainer'>
-                    <ChatlistContent />
+                <ChatlistContent chatContent={this.chatlistContent} />
                 </div>
             </div>
             <div className='chatWindow-container'>
@@ -129,12 +113,63 @@ render (){
                                 send message
                                 </button>
                             </div>
-                    </div>
+                      </div>
                     </div>
                 </div>
             </div>
         </div>
         )};
     }
+
+    class ChatlistContent extends Main {
+      constructor(props) {
+        super(props);
+        this.state = {
+          isLoading: true,
+          data: [],
+        };
+        this.navigate = this.props.navigate;
+        window.whoamiResult = whoami(this.navigate);
+      }
+      
+      
+      componentDidMount() {
+        fetch("/api/message/chatList?id=1")
+          .then((response) => response.json())
+          .then((data) => {
+            this.setState({ data: data, isLoading: false });
+          });
+          const myRef = this.chatContent;
+          console.log(myRef);
+      }
+    
+      render() {
+        const { isLoading, data } = this.state;
+    
+        if (isLoading) {
+          return <div>Loading...</div>;
+        } else {
+          const userList = data.result.map((item) => {
+            const lastMessage = item.messages || {};
+            return (
+              <div
+                onClick={() => {
+                  window.setupChatId(item.chatid);
+                }}
+                className="chatList-content"
+                key={item.chatid}
+              >
+                <img id="userAvatar" src={avatar}></img>
+                <p id="userName">{lastMessage.author || ""}</p>
+                <p id="lastMessageTime">{lastMessage.time || ""}</p>
+                <p id="lastMessageText">{lastMessage.text || ""}</p>
+              </div>
+            );
+          });
+          return <React.Fragment ref={this.chatContent}> {userList} </React.Fragment>;
+        }
+      }
+    }
+
 
 export default Main;
